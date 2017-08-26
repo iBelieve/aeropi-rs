c4f81b76d4d7e16288f6d1913ce9f04a71aa144a
Michael Spencer <sonrisesoftware@gmail.com>
2017-08-25 21:19:40 -0500

    Initial code to calculate the target acceleration using PID controllers

--- src/controller.rs
+++ src/controller.rs
@@ -1,12 +1,16 @@
 use config::Calibration;
-use math::{rotate_vector, EulerAngles, Vec3};
+use math::{EulerAngles, Vec3, rotate_vector};
 use motors::Motors;
+use pid_control::{Controller, PIDController};
 use sensors::Sensors;
 
 pub struct FlightController {
     sensors: Sensors,
     motors: Motors,
-    target_velocity: Vec3
+    target_velocity: Vec3,
+    aligned_vx_pid: PIDController,
+    aligned_vy_pid: PIDController,
+    quad_vz_pid: PIDController,
 }
 
 impl FlightController {
@@ -14,7 +18,11 @@ impl FlightController {
         FlightController {
             sensors: Sensors::new(),
             motors: Motors::new(),
-            target_velocity: Vec3::zero()
+            target_velocity: Vec3::zero(),
+            // TODO: Pick the right constants for the three PIDs
+            aligned_vx_pid: PIDController::new(1.0, 1.0, 1.0),
+            aligned_vy_pid: PIDController::new(1.0, 1.0, 1.0),
+            quad_vz_pid: PIDController::new(1.0, 1.0, 1.0),
         }
     }
 
@@ -36,11 +44,28 @@ impl FlightController {
         println!("Flight controller calibrated.");
     }
 
+    #[allow(dead_code)] // TODO: Remove once runloop is implement
     pub fn step(&mut self) {
+        let dt: f64 = 0.01; // TODO: Properly calculate this
         let readings = self.sensors.read();
         let angles = EulerAngles::new(readings.pitch, readings.roll, readings.yaw);
 
         let quad_v = angles.inertial_to_body(self.target_velocity);
-        let (aligned_vx, aligned_vy) = rotate_vector(self.target_velocity.x, self.target_velocity.y, readings.yaw);
+        let (aligned_vx, aligned_vy) =
+            rotate_vector(self.target_velocity.x, self.target_velocity.y, readings.yaw);
+
+        // Calculate the xyz acceleration based on the requested/current xyz velocity
+        // Notes:
+        //  - The x/y acceleration is "aligned", or rotated so x always points forward and y always points right. This
+        //    is so the aligned velocity/acceleration is always in the same frame as pitch and roll
+        //  - The vertical acceleration is in the quad frame because we'll be controlling the acceleration
+        //    directly up and down (by changing the power of the props), which are in the quad frame.
+        self.aligned_vx_pid.set_target(aligned_vx);
+        self.aligned_vy_pid.set_target(aligned_vy);
+        self.quad_vz_pid.set_target(quad_v.z);
+
+        let aligned_accel_x = self.aligned_vx_pid.update(readings.aligned_vx, dt);
+        let aligned_accel_y = self.aligned_vy_pid.update(readings.aligned_vy, dt);
+        let quad_accel_z = self.quad_vz_pid.update(readings.quad_vz, dt);
     }
 }
--- src/sensors/accelerometer.rs
+++ src/sensors/accelerometer.rs
@@ -2,7 +2,8 @@ use std::{thread, time};
 use i2cdev::core::*;
 use i2cdev::linux::{LinuxI2CDevice, LinuxI2CError};
 use config::AccelerometerCalibration;
-use math::{twos_comp_combine, Vec3};
+use math::{Vec3, twos_comp_combine};
+
 
 const CALIBRATION_ITERATIONS: u8 = 50;
 const ACCELERATION_SCALE_FACTOR: f64 = 0.001;
@@ -15,24 +16,24 @@ const LSM_WHOAMI_CONTENTS: u8 = 0b1001001; // Device self-id
 // LSM_WHOAMI_CONTENTS = 0b1001001  // Device self-id
 
 // const CTRL_0: u8 = 0x1F;  // General settings
-const CTRL_1: u8 = 0x20;  // Turns on accelerometer and configures data rate
-const CTRL_2: u8 = 0x21;  // Self test accelerometer, anti-aliasing accel filter
+const CTRL_1: u8 = 0x20; // Turns on accelerometer and configures data rate
+const CTRL_2: u8 = 0x21; // Self test accelerometer, anti-aliasing accel filter
 // const CTRL_3: u8 = 0x22;  // Interrupts
 // const CTRL_4: u8 = 0x23;  // Interrupts
-const CTRL_5: u8 = 0x24;  // Turns on temperature sensor
+const CTRL_5: u8 = 0x24; // Turns on temperature sensor
 // const CTRL_6: u8 = 0x25;  // Magnetic resolution selection, data rate config
 // const CTRL_7: u8 = 0x26;  // Turns on magnetometer and adjusts mode
 
-const ACC_X_LSB: u8 = 0x28;  // x
+const ACC_X_LSB: u8 = 0x28; // x
 const ACC_X_MSB: u8 = 0x29;
-const ACC_Y_LSB: u8 = 0x2A;  // y
+const ACC_Y_LSB: u8 = 0x2A; // y
 const ACC_Y_MSB: u8 = 0x2B;
-const ACC_Z_LSB: u8 = 0x2C;  // z
+const ACC_Z_LSB: u8 = 0x2C; // z
 const ACC_Z_MSB: u8 = 0x2D;
 
 pub struct Accelerometer {
     i2c: LinuxI2CDevice,
-    offsets: Vec3
+    offsets: Vec3,
 }
 
 impl Accelerometer {
@@ -40,16 +41,17 @@ impl Accelerometer {
         Accelerometer {
             i2c: LinuxI2CDevice::new("/dev/i2c-1", LSM_ADDRESS)
                 .expect("Unable to connect to the i2c bus"),
-            offsets: Vec3::zero()
+            offsets: Vec3::zero(),
         }
     }
 
     fn enable(&mut self) -> Result<(), LinuxI2CError> {
-        self.i2c.smbus_write_byte_data(CTRL_1, 0b10010111)?;  // enable accelerometer, 800 hz sampling
-        self.i2c.smbus_write_byte_data(CTRL_2, 0x00)?;  // set +/- 2g full scale
-        self.i2c.smbus_write_byte_data(CTRL_5, 0b01100100)?;  // high resolution mode, thermometer off, 6.25hz ODR
+        self.i2c.smbus_write_byte_data(CTRL_1, 0b10010111)?; // enable accelerometer, 800 hz sampling
+        self.i2c.smbus_write_byte_data(CTRL_2, 0x00)?; // set +/- 2g full scale
+        self.i2c.smbus_write_byte_data(CTRL_5, 0b01100100)?; // high resolution mode, thermometer off, 6.25hz ODR
         // self.i2c.smbus_write_byte_data(CTRL_6, 0b00100000)  // set +/- 4 gauss full scale
         // self.i2c.smbus_write_byte_data(CTRL_7, 0x00)  // get magnetometer out of low power mode
+
         Ok(())
     }
 
@@ -97,7 +99,7 @@ impl Accelerometer {
             y: twos_comp_combine(self.i2c.smbus_read_byte_data(ACC_Y_MSB)?,
                                  self.i2c.smbus_read_byte_data(ACC_Y_LSB)?) as f64,
             z: twos_comp_combine(self.i2c.smbus_read_byte_data(ACC_Z_MSB)?,
-                                 self.i2c.smbus_read_byte_data(ACC_Z_LSB)?) as f64
+                                 self.i2c.smbus_read_byte_data(ACC_Z_LSB)?) as f64,
         })
     }
 }
